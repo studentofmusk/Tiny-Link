e2085d51ad3b4e18c572490cfba4875db50ca710
unknown <farooqvrofficial@gmail.com>
2025-11-20 09:41:38 +0530

    db config updated

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,11 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+     ssl: {
+        require: true,
+        rejectUnauthorized: false, // required for Supabase external connections
+    },
 })
 
 module.exports = pool;
