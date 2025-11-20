6f59f358c0b17fd3e7a94676cb20c75d61debaec
unknown <farooqvrofficial@gmail.com>
2025-11-20 09:08:23 +0530

    updated server

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config({path:['.env']});
 const app = require('./app');
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>{
     console.log(`server running at port: ${PORT}`)
